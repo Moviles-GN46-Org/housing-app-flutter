5356ed4c2e90768e169a880ecb850390a58c8c16
almanza33 <d.almanza@uniandes.edu.co>
2026-03-20 14:27:30 -0500

    Revert "Merge pull request #3 from Moviles-GN46-Org/feature/auth-login"
    This reverts commit 2708875cc43f8817fe5cd62ed8dcc1b975dec657, reversing
    changes made to 9be5ebda483443be43157b465f8c001e80dcfe34.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:housing_app_flutter/main.dart'; // Asegura tu ruta aquí
+
+import 'package:housing_app_flutter/main.dart';
 
 void main() {
-  testWidgets('La app compila e inicializa Firebase', (WidgetTester tester) async {
-    // Construimos nuestra nueva app CasAndesApp en lugar de MyApp
-    await tester.pumpWidget(const CasAndesApp());
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
-    // Verificamos que el texto que pusimos en el main esté en pantalla
-    expect(find.textContaining('Firebase Conectado'), findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
